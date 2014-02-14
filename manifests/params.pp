2a97fe39d4b2ae490f26d9d5e5e3b1558cce46ec
Duncan Booth <kupuguy@gmail.com>
2014-02-14 11:23:48 +0000

    Change the default rescheduleWaitTime so it is a valid value. According to docs valid values are in range 1-60, so the original 0 wasn;t valid.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@ class windows_autoupdate::params {
     $scheduledInstallDay = hiera('au_scheduledInstallDay','1')
     $scheduledInstallTime = hiera('au_scheduledInstallTime','1')
     $useWUServer = hiera('au_useWUServer','0')
-    $rescheduleWaitTime = hiera('au_rescheduleWaitTime','0')
+    $rescheduleWaitTime = hiera('au_rescheduleWaitTime','10')
     $noAutoRebootWithLoggedOnUsers = hiera('au_noAutoRebootWithLoggedOnUsers','0')
     $p_reg_key = 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU'
 }
